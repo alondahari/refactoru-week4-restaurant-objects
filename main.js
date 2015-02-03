b0014936131b98df33ea867a104c63329f103f18
Alon Dahari <climbinghobo@gmail.com>
2015-02-03 14:16:56 -0700

    add FoodItem

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-$(document).on('ready', function() {
-  
-});
+var FoodItem = function (name, calories, vegan, glutenFree, citrusFree) {
+	this.name = name;
+	this.calories = calories;
+	this.vegan = vegan;
+	this.glutenFree = glutenFree;
+	this.citrusFree = citrusFree;
+}
+
+FoodItem.prototype.toString = function () {
+	var vegan = this.vegan ? 'vegan' : 'not vegan',
+			glutenFree = this.glutenFree ? 'gluten free' : 'contains gluten',
+			citrusFree = this.citrusFree ? 'citrus free' : 'contains citrus';
+	return 'name: ' + this.name +
+		', calories: ' + this.calories +
+		', dietary information: ' + vegan + ', ' + glutenFree + ', ' + citrusFree;
+}
